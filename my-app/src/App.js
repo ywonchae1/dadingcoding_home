5ad2b30ca9a5bf65bb6d4a42e4ab3bd56bef7f6f
ywonchae1 <ywonchae62@gmail.com>
2023-06-23 13:00:37 +0900

    my-app 폴더 오류

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './css/App.css';
+import Navi from './Navi';
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <Navi></Navi>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
