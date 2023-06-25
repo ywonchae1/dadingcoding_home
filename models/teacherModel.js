72619482284b9df9b4fc09913d9e542d75993590
ywonchae1 <ywonchae62@gmail.com>
2023-06-25 18:02:07 +0900

    #feat: 선생님 페이지 생성 및 정보 추출 구현

--- models/teacherModel.js
+++ models/teacherModel.js
@@ -2,14 +2,17 @@
 const db = require("../config/db.js");
 
 module.exports = {
+    //id로 계정 정보 가져오기
     getInfoById: async(id) => {
         try {
             let rawQuery = `
             SELECT *
-            FROM accounts
-            WHERE id = ?;`;
-            let res = db.query(rawQuery, [id]);
-            return res[0];
+            FROM teachers
+            JOIN accounts
+            ON t_id=id
+            WHERE id=?;`;
+            let res = await db.query(rawQuery, [id]);
+            return res[0][0]; //정보의 묶음이 하나임
         } catch(err) {
             return err;
         }
