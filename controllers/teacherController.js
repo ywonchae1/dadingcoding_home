0e3925bf533321ab89910361b48d979cb936b7cc
ywonchae1 <ywonchae62@gmail.com>
2023-06-26 11:15:55 +0900

    #1 feat: 개인정보와 수업정보 가져오기 구현

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,8 +1,31 @@
 const teacherModel = require("../models/teacherModel.js");
 
+//나중에 쿠키로 변경해야 함
+const id = 'ywonchae1';
+
 module.exports = {
     teacherPage: async(req, res) => {
-        let teacherInfo = await teacherModel.getInfoById(req.params.tid);
-        res.render('user/userLayout.ejs', {info: teacherInfo});
+        let teacherInfo = await teacherModel.getInfoById(id);
+        res.render(
+            'user/userLayout.ejs',
+            {
+                content: 'teacher/settings.ejs',
+                info: teacherInfo
+            }
+        );
+    },
+
+    teacherSchedule: async(req, res) => {
+        let teacherSchedules = await teacherModel.getScheduleById(id);
+        res.render(
+            'user/userLayout.ejs',
+            {
+                content: 'teacher/schedules.ejs',
+                info: teacherSchedules
+            }
+        );
+    },
+
+    teacherRegister: async(req, res) => {
     }
 };
